e3aab8e5dcbf63b9f8b58faef4fbe7ac852690ee
Hsin-lin Cheng <lancetw@gmail.com>
2015-01-15 19:25:24 +0800

    支援搜尋預設值

--- js/app/search.js
+++ js/app/search.js
@@ -1,6 +1,9 @@
 require(['bootstrap-dropdown'], function() {
   $('#search a').on('click', function() {
-    $("#search").find('.input.form-control').attr('placeholder', $(this).text() + '搜尋');
+    $('#search').find('.input.form-control').attr('placeholder', $(this).text() + '搜尋');
     $('#search').find('input[name="area"]').attr('value', $(this).attr('data-area'));
   });
+
+  var area = $('#search').find('.input.form-control').attr('data-area');
+  $($('#search .dropdown-menu').find('a[data-area="'+ area +'"]')).trigger('click');
 });
